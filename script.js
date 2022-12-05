98cca518abdf0a8aec465c025954f0d4e9a9e4fa
djremail <borol.igor@gmail.com>
2022-12-05 13:30:08 +0200

    first commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const text = document.querySelector('.container_text');
 const image = document.querySelector('.container_img');
 let hours = new Date().getHours();
 
-if(hours == 8 ||hours == 9 || hours == 14 || hours == 15 || hours == 20 || hours == 21){
+if(hours == 7 ||hours == 8 || hours == 13 || hours == 14 || hours == 19 || hours == 20){
     page.style.backgroundColor = '#464545';
     title.textContent = 'Наразі у Миколаєві немає світла'
     image.src='img/301.png'
@@ -18,12 +18,12 @@ if(hours == 8 ||hours == 9 || hours == 14 || hours == 15 || hours == 20 || hours
 
 function getTimeToTheLight(){
     let time = '';
-    if(hours >= 8 && hours < 11){
-        time = 'Чекаємо 10 години'
-    }else if (hours >= 14 && hours < 17){
-        time = 'Чекаємо 16 години'
-    }else if (hours >= 20 && hours < 23){
-        time = 'Чекаємо 22 години'
+    if(hours >= 7 && hours < 10){
+        time = 'Чекаємо до 9:00'
+    }else if (hours >= 13 && hours < 16){
+        time = 'Чекаємо до 15:00'
+    }else if (hours >= 19 && hours < 22){
+        time = 'Чекаємо 21:00'
     }
 
     return time
@@ -31,12 +31,12 @@ function getTimeToTheLight(){
 
 function getTimeToNotLight(){
     let newTime = '';
-    if(hours >= 0 && hours < 8){
-        newTime = 'Очікуваний час відключення - 08:00'
-    }else if (hours >= 10 && hours < 14){
-        newTime = 'Очікуваний час відключення - 14:00'
-    }else if (hours >= 16 && hours < 20){
-        newTime = 'Очікуваний час відключення - 20:00'
+    if(hours >= 0 && hours < 7){
+        newTime = 'Очікуваний час відключення - 07:00'
+    }else if (hours >= 9 && hours < 13){
+        newTime = 'Очікуваний час відключення - 13:00'
+    }else if (hours >= 15 && hours < 19){
+        newTime = 'Очікуваний час відключення - 19:00'
     }
 
     return newTime
